3b8381ab54bbe3cdec5a6b8f46d4385b6006e748
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-30 12:01:50 -0300

    ok

--- pessoa/models.py
+++ pessoa/models.py
@@ -8,3 +8,5 @@ class Pessoa(models.Model):
     nome_completo = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     ativo = models.BooleanField(default=True)
+    data_nascimento = models.DateField()
+    data_cadastro = models.DateField(auto_now_add=True)
