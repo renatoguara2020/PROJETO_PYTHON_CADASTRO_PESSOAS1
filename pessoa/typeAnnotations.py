f3d1f15d71a6469310e5e898d8e0e9df7932818c
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-04 09:59:57 -0300

    ok

---
+++ pessoa/typeAnnotations.py
@@ -0,0 +1,16 @@
+# Built-in class examples
+an_int: int = 3
+a_float: float = 1.23
+a_str: str = 'Hello'
+a_bool: bool = False
+a_list: list = [1, 2, 3]
+a_set: set = set([1, 2, 3])  # or {1, 2, 3}
+a_dict: dict = {'a': 1, 'b': 2}
+
+
+idade = 51
+ano_atual = 2022
+
+idade1 = ano_atual - idade
+
+print(idade1)
