ede77a5d216fc3a29891924f79ccbfb9ff1b9605
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-10 13:59:37 -0300

    ok

---
+++ pessoa/arrays.php
@@ -0,0 +1,15 @@
+<?php
+ $vetor [1] = "Alemanha ";
+ $vetor [2] = "Italia ";
+ $vetor [3] = "França";
+ $vetor ["testando"] = 1;
+?>
+
+<?php
+ $vetor = array(1 => "Alemanha", 2 => "Italia", 3 => "França", "testando" => 1);
+?>
+
+<?php
+ $vetorPaises = array("Alemanha", "Italia", "França", "Noruega");
+ echo $vetorPaises[1];
+?>
